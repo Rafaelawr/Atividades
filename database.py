34a39439c8f90e6b164d1ff4cacbe514784b9ae5
Rafaelawr <rafaelawr@hotmail.com>
2025-05-26 11:10:09 -0300

    atualizando db

--- database.py
+++ database.py
@@ -1,3 +1,9 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
