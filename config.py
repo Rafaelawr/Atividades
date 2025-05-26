f34b5c95a8e43a6bf972a96c42390baff413f440
Rafaelawr <rafaelawr@hotmail.com>
2025-05-26 02:38:02 -0300

    Config modficiado

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from database import db
 
-def create_app():
-    app = Flask(__name__)
-    app.config['DEBUG'] = True
-    return app
+app= Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///atividade.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
+db.init_app(app)
