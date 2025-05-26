a86c1d5b446386792bc699b5227d12b590358dad
Rafaelawr <rafaelawr@hotmail.com>
2025-05-26 03:37:51 -0300

    Adicionado route_atividade.py com endpoints iniciais

--- controllers/route_atividade.py
+++ controllers/route_atividade.py
@@ -1,8 +1,38 @@
 from flask import Blueprint, request, jsonify
-from  atividade_model import Atividade
+from  models import atividade_model 
+import database import db
 import requests
 
-bp_atividade = Blueprint('atividade', __name__)
+routes = Blueprint('routes', __name__)
 
-@bp_atividade.route('/reservas', methods=['GET'])
+bp_atividade = Blueprint('bp_atividade', __name__)
 
+@routes.route('/atividade', methodos=['POST'])
+def criar_atividade():
+    dados = request.json
+    try:
+        nova_atividade = atividade_model.criar_atividade(dados)
+        return jsonify(nova_atividade), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+@bp_atividade.route('/atividade', methods=['POST']) 
+
+
+
+@bp_atividade.route('/atividade', methods=['GET'])
+def listar_atividade():
+    atividades = atividade_model.listar_atividade()
+    return jsonify(atividades)
+
+
+@atividade_bp.route('/<int:id_atividade>', methods=['GET'])
+
+@bp_atividade.route('/atividade/<int:id_atividade>', methods=['GET'])
+def buscar_atividade(id_atividade):
+    try:
+        atividade = atividade_model.buscar_atividade(id_atividade)
+        return jsonify(atividade)
+    except atividade_model.AtividadeNotFound:
+        return jsonify({'error': 'Atividade não encontrada'}), 404
+
+@bp_atividade.route('/<int:id_atividade>/professor/<int:id_professor>', methods=['GET'])
