5e082459e682f7e692760c6dd6f0aa486109eb92
Rafaelawr <rafaelawr@hotmail.com>
2025-05-26 11:41:02 -0300

    app add

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from database import app, db
 from controllers.route_atividade import bp_atividade
 from flask_sqlalchemy import SQLAlchemy
 
-app.register_blueprint(routes)
+app.register_blueprint(bp_atividade)
 
 with app.app_context():
     db.create_all()
